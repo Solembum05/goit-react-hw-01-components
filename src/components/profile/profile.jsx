2f843465ec126cee2f6d9946b8d08bd436455b1b
Kysil Ivan <kysilivan1997@gmail.com>
2023-08-24 10:56:30 +0200

    try styled

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,30 +1,32 @@
 import PropTypes from 'prop-types';
-import css from './profile.module.css';
+import { Avatar, Card, Description, Label, Location, Name, Quantity, Stats, StatsItem, Tag } from './profile.styled';
+// import css from './profile.module.css';
+
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div className={css.profile}>
-      <div className={css.description}>
-        <img src={avatar} alt="User avatar" className={css.avatar} />
-        <p className={css.name}>{username}</p>
-        <p className={css.tag}>@{tag}</p>
-        <p className={css.location}>{location}</p>
-      </div>
-      <ul className={css.stats}>
-        <li className={css.statsItem}>
-          <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{stats.followers}</span>
-        </li>
-        <li className={css.statsItem}>
-          <span className={css.label}>Views</span>
-          <span className={css.quantity}>{stats.views}</span>
-        </li>
-        <li className={css.statsItem}>
-          <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+    <Card>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+      <Stats>
+        <StatsItem>
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </StatsItem>
+      </Stats>
+    </Card>
   );
 };
 
--- src/components/profile/profile.styled.js
+++ src/components/profile/profile.styled.js
@@ -1,62 +1,60 @@
-.profile {
+import styled from "styled-components";
+
+export const Card = styled.div`
   font-size: 30px;
   width: 300px;
   padding-top: 50px;
-      padding-left: 50px;
-    padding-right: 50px;
+  padding-left: 50px;
+  padding-right: 50px;
   margin: 0 auto;
   border: 1px solid black;
   border-radius: 5%;
-}
+`;
 
-.description {
+export const Description = styled.div`
   display: block;
   text-align: center;
-}
+`;
 
-.avatar {
+export const Avatar = styled.img`
   border-radius: 50%;
   border: 1px solid black;
   margin-bottom: 40px;
   background-color: gray;
-}
+`;
 
-.name {
+export const Name = styled.p`
   font-weight: 700;
   font-size: 32px;
   margin-bottom: 20px;
-}
+`;
 
-.tag {
+export const Tag = styled.p`
   color: gray;
   margin-bottom: 20px;
-}
+`;
 
-.location {
+export const Location = styled.p`
   color: gray;
   margin-bottom: 40px;
-}
+`;
 
-.stats {
+export const Stats = styled.ul`
   width: 100%;
-    gap: 10px;
+  gap: 10px;
   display: flex;
+`;
 
-}
-
-.statsItem {
-
+export const StatsItem = styled.li`
   display: flex;
   flex-direction: column;
-}
+`;
 
-.label {
+export const Label = styled.span`
   margin-bottom: 10px;
-}
+`;
 
-.quantity {
+
+export const Quantity = styled.span`
   margin-bottom: 10px;
-}
-/* 
-label
-quantity */
+`;
